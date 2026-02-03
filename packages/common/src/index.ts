ede43d61169a0b4473290fd065f8d5e14296ad53
shayandutta <shayandutta23@gmail.com>
2026-02-03 22:47:19 +0530

    feat: added some comments

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,3 +1,5 @@
+// re-export all the common utilities
+
 export * from "./logger";
 export * from "./env";
-export type {Logger} from "pino";
+export type {Logger} from "pino"; // why do we need to export the Logger type? -> because we want to use the Logger type in the other services
